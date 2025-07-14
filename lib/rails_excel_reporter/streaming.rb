955f50fea2c1961fb088133eb2236d95976f1874
Eli Herrera <esrbastianherrera@gmail.com>
2025-07-14 15:08:06 -0600

    feat: add streaming module to RailsExcelReporter for data processing

---
+++ lib/rails_excel_reporter/streaming.rb
@@ -0,0 +1,81 @@
+module RailsExcelReporter
+  module Streaming
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def streaming_threshold
+        @streaming_threshold || RailsExcelReporter.config.streaming_threshold
+      end
+
+      def streaming_threshold=(value)
+        @streaming_threshold = value
+      end
+    end
+
+    def should_stream?
+      collection_size >= self.class.streaming_threshold
+    end
+
+    def collection_size
+      return @collection_size if defined?(@collection_size)
+
+      @collection_size = if @collection.respond_to?(:count)
+                           @collection.count
+                         elsif @collection.respond_to?(:size)
+                           @collection.size
+                         elsif @collection.respond_to?(:length)
+                           @collection.length
+                         else
+                           @collection.to_a.size
+                         end
+    end
+
+    def stream_data(&block)
+      return enum_for(:stream_data) unless block_given?
+
+      if should_stream?
+        stream_large_dataset(&block)
+      else
+        stream_small_dataset(&block)
+      end
+    end
+
+    def with_progress_tracking
+      return enum_for(:with_progress_tracking) unless block_given?
+
+      total = collection_size
+      current = 0
+
+      stream_data do |item|
+        current += 1
+        progress = OpenStruct.new(current: current, total: total, percentage: (current.to_f / total * 100).round(2))
+
+        @progress_callback&.call(progress)
+
+        yield item, progress
+      end
+    end
+
+    private
+
+    def stream_large_dataset(&block)
+      if @collection.respond_to?(:find_each)
+        @collection.find_each(batch_size: 1000, &block)
+      elsif @collection.respond_to?(:each)
+        @collection.each(&block)
+      else
+        @collection.to_a.each(&block)
+      end
+    end
+
+    def stream_small_dataset(&block)
+      if @collection.respond_to?(:each)
+        @collection.each(&block)
+      else
+        @collection.to_a.each(&block)
+      end
+    end
+  end
+end
